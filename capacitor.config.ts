5e327bfdb8eea6e9bc5631824ffbdd237e0c3dc9
dansimoes6-create <dansimoes6@gmail.com>
2026-08-16 00:09:20 -0300

    Adiciona capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = { appId: 'com.studioflow.gestao', appName: 'StudioFlow', webDir: 'www', server: { androidScheme: 'https' } };
+export default config;
